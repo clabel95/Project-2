f299c917e0627579b2d1f2597bf7567c85521b93
Maribel Montes <maribel.doris@gmail.com>
2022-02-14 13:57:44 -0600

    removed the modifications on recipes and index.js

--- models/index.js
+++ models/index.js
@@ -2,6 +2,6 @@ const User = require('./User');
 const User_recipes = require('./User_recipes');
 const Recipes = require('./Recipes');
 
-// module.exports = { User_recipes };
+module.exports = { User_recipes };
 module.exports = { Recipes };
-// module.exports = { User };
+module.exports = { User };
