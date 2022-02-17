d5fd5807dbdc69450d9237f5b644669d32d3d5fc
Clay Abel <clayton.abel.95@gmail.com>
2022-02-17 10:38:18 -0600

    heroku update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
+const {engine} = require('express-handlebars');
 const session = require('express-session');
-const hbs = exphbs.create({});
+// const hbs = exphbs.create({});
 const path = require('path');
 
 // const session = require('express-session');
@@ -12,6 +12,10 @@ const routes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static(path.join(__dirname, 'public')));
+}
 
 const sess = {
     secret: 'secrets',
